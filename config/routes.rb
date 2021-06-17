787458e61e3bbddee8c6c950eab7886a4b2d5bcd
martin drenovac <martin.drenovac@gmail.com>
2021-06-17 20:39:07 +1000

    static public controller with pages, routers.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_public#landing_page'
+#  get 'static_public/landing_page' # this is the generic form
+#  get 'static_public/privacy'      # convert this to more sensible looking paths
+  get 'privacy', to: 'static_public#privacy'
+#  get 'static_public/terms'
+  get 'terms', to: 'static_public#terms'  
 end
